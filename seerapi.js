da5f97a4819cacb1b2b11485e84adb3eb56d384b
Jonas Almeida <jalmeida@mathbiol.org>
2020-02-10 08:29:16 -0500

    Update seerapi.js

--- seerapi.js
+++ seerapi.js
@@ -5,3 +5,20 @@ seerapi={ini:Date()}
 if(typeof(define)!='undefined'){
     define(seerapi)
 }
+
+// in case there is a web context
+
+seerapi.keyInput=document.getElementById("seerAPIkey")
+if(seerapi.keyInput){
+    if(localStorage.seerAPIkey){
+        seerapi.keyInput.value=localStorage.seerAPIkey
+    }
+    seerapi.keyInput.onkeyup=ev=>{
+        readCheck='<span style:"font-size:x-small">(then press Enter to save)</span>'
+        if(ev.keyCode==13){
+            console.log(`new seer key logged at ${Date()}`)
+            localStorage.seerAPIkey=seerapi.keyInput.value
+        }
+        //debugger &#x2714;
+    }
+}
